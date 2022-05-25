7ce754ab6d50322d88039e986e28116a61600765
Niklas Jaeger <niklas.jaeger97@gmail.com>
2022-05-25 19:07:00 +0200

    add frc

--- src/denoise.py
+++ src/denoise.py
@@ -30,6 +30,90 @@ cmdline.add_argument('--format', type=str, default='jpg', help='output image for
 cmdline.add_argument('--fps', type=int, default=24, help='output video frame rate')
 cmdline.add_argument('-f','--features', type=int, default=[], nargs='*', help='Tuples of feature channels to select from input')
 
+def fourier_ring_correlation(image_1, image_2, bin_width=2):
+    
+    image_1 = image_1 / np.sum(image_1)
+    image_2 = image_2 / np.sum(image_2)
+    f1, f2 = np.fft.fft2(image_1), np.fft.fft2(image_2)
+    af1f2 = np.real(f1 * np.conj(f2))
+    af1_2, af2_2 = np.abs(f1)**2, np.abs(f2)**2
+    nx, ny = af1f2.shape
+    x = np.arange(-np.floor(nx / 2.0), np.ceil(nx / 2.0))
+    y = np.arange(-np.floor(ny / 2.0), np.ceil(ny / 2.0))
+    distances = list()
+    wf1f2 = list()
+    wf1 = list()
+    wf2 = list()
+    for xi, yi in np.array(np.meshgrid(x,y)).T.reshape(-1, 2):
+        distances.append(np.sqrt(xi**2 + xi**2))
+        xi = int(xi)
+        yi = int(yi)
+        wf1f2.append(af1f2[xi, yi])
+        wf1.append(af1_2[xi, yi])
+        wf2.append(af2_2[xi, yi])
+
+    bins = np.arange(0, np.sqrt((nx//2)**2 + (ny//2)**2), bin_width)
+    f1f2_r, bin_edges = np.histogram(
+        distances,
+        bins=bins,
+        weights=wf1f2
+    )
+    f12_r, bin_edges = np.histogram(
+        distances,
+        bins=bins,
+        weights=wf1
+    )
+    f22_r, bin_edges = np.histogram(
+        distances,
+        bins=bins,
+        weights=wf2
+    )
+    density = f1f2_r / np.sqrt(f12_r * f22_r)
+    return density, bin_edges
+def add_color_val_difference(img_input, img_pred,img_target):
+  rows = 3
+  columns = 3
+  fig  = plt.figure(figsize=((512*3)/80, (512*2)/80))
+  fig.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
+  fig.add_subplot(rows, columns, 1)
+  plt.imshow(img_input.cpu().permute(0,2,3,1).numpy()[0])
+  plt.axis('off')
+  plt.title("Input")
+  fig.add_subplot(rows, columns, 2)
+  plt.imshow(img_pred.cpu().permute(0,2,3,1).numpy()[0])
+  plt.axis('off')
+  plt.title("Prediction")
+  fig.add_subplot(rows, columns, 3)
+  plt.imshow(img_target.cpu().permute(0,2,3,1).numpy()[0])
+  plt.axis('off')
+  plt.title("Target")
+  
+  fig.add_subplot(rows, columns, 4)
+  for i, col in enumerate(['b', 'g', 'r']):
+      im=cv2.normalize(cv2.cvtColor(np.transpose(img_input.cpu().numpy()[0][0:3], [1, 2, 0]), cv2.COLOR_RGB2BGR), None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
+      hist = cv2.calcHist([im], [i], None, [256], [0.0, 256.0])
+      plt.plot(hist, color = col)
+      plt.xlim([0, 260])
+      plt.title("Color Hist Input")
+  fig.add_subplot(rows, columns, 5)
+  for i, col in enumerate(['b', 'g', 'r']):
+      im=cv2.normalize(cv2.cvtColor(np.transpose(img_pred.cpu().numpy()[0][0:3], [1, 2, 0]), cv2.COLOR_RGB2BGR), None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
+      hist = cv2.calcHist([im], [i], None, [256], [0.0,256.0])
+      plt.plot(hist, color = col)
+      plt.xlim([0, 260])
+      plt.title("Color Hist Prediction")
+  fig.add_subplot(rows, columns, 6)
+  for i, col in enumerate(['b', 'g', 'r']):
+      im=cv2.normalize(cv2.cvtColor(np.transpose(img_target.cpu().numpy()[0][0:3], [1, 2, 0]), cv2.COLOR_RGB2BGR), None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
+      hist = cv2.calcHist([im], [i], None, [256], [0.0, 256.0])
+      plt.plot(hist, color = col)
+      plt.xlim([0, 260])
+      plt.title("Color Hist Target")
+  plt.tight_layout()
+  plt.savefig([f'{name}/{name}_color_diff{args.batch_size*idx+j:06}.jpg' for j in range(frame.size(0))][0], bbox_inches='tight', pad_inches=0, dpi=500, pil_kwargs={'quality':95})
+  plt.close()
+  plt.cla()
+  plt.clf()
 # -----------------------------------------------------------
 # MAIN
 
@@ -98,12 +182,9 @@ if __name__ == "__main__":
                 data.write([f'{name}/{name}_input{args.batch_size*idx+j:06}.hdr' for j in range(frame.size(0))], img_input.cpu().numpy())
                 # metrics
                 #color_val_difference
-                diff_red = np.abs(img_target.cpu().numpy()[0,0,...]-img_pred.cpu().numpy()[0,0,...])
-                diff_green = np.abs(img_target.cpu().numpy()[0,1,...]-img_pred.cpu().numpy()[0,1,...])
-                diff_blue = np.abs(img_target.cpu().numpy()[0,2,...]-img_pred.cpu().numpy()[0,2,...])
-                f = open(f'{name}/{name}col_diff.txt', "w")
-                f.write(f'Red:{np.mean(diff_red)} Green:{np.mean(diff_green)} Blue:{np.mean(diff_blue)}')
-                f.close()
+                add_color_val_difference(img_input, img_pred,img_target)
+                
+                
                 mse_input_target = 0
                 ssim_input_target = 0
                 mse_pred_target = 0
@@ -111,6 +192,8 @@ if __name__ == "__main__":
                 # frequency domain images
                 rgb_weights = [0.2989, 0.5870, 0.1140]
                 grayscale = cv2.cvtColor(img_pred.cpu().permute(0,2,3,1).numpy()[0], cv2.COLOR_RGB2GRAY)[np.newaxis,...]
+                
+                
                 f_pred = np.fft.fft2(grayscale)
                 magnitude_spectrum_pred = 20* np.log(np.abs(np.fft.fftshift(f_pred)))
                 data.write([f'{name}/{name}_pred_frequency_spectrum{args.batch_size*idx+j:06}.jpg' for j in range(frame.size(0))], magnitude_spectrum_pred[:,np.newaxis,...],1)
@@ -122,6 +205,32 @@ if __name__ == "__main__":
                 f_target = np.fft.fft2(grayscale)
                 magnitude_spectrum_target = 20* np.log(np.abs(np.fft.fftshift(f_target)))
                 data.write([f'{name}/{name}_target_frequency_spectrum{args.batch_size*idx+j:06}.jpg' for j in range(frame.size(0))], magnitude_spectrum_target[:,np.newaxis,...],1)
+                
+
+                
+                
+                # plt.title('FRC')
+                # input_gray = cv2.cvtColor(img_input.cpu().permute(0,2,3,1).numpy()[0], cv2.COLOR_RGB2GRAY)
+                # pred_gray =  cv2.cvtColor(img_pred.cpu().permute(0,2,3,1).numpy()[0], cv2.COLOR_RGB2GRAY)
+                # target_gray = cv2.cvtColor(img_target.cpu().permute(0,2,3,1).numpy()[0], cv2.COLOR_RGB2GRAY)
+                # im2_pred = img_pred.cpu().numpy()[0][1::2].sum(axis=0)
+
+                # im1_input = img_input.cpu().numpy()[0][::2].sum(axis=0)
+                # im2_input = img_input.cpu().numpy()[0][1::2].sum(axis=0)
+
+                # im1_target = img_target.cpu().numpy()[0][::2].sum(axis=0)
+                # im2_target = img_target.cpu().numpy()[0][1::2].sum(axis=0)
+                
+                # frc_input_pred, frc_bins = fourier_ring_correlation(input_gray,pred_gray)
+
+                # frc_input_target, frc_bins = fourier_ring_correlation(input_gray,target_gray)
+                
+                # plt.plot(frc_input_pred, label="Input Pred", color="red")
+                # plt.plot(frc_input_target, label="Input Target", color="blue")
+          
+               # plt.show()
+
+
                 for j in range(frame.size(0)):
                     mse_input_target += losses.mse(img_input, img_target).item()
                     ssim_input_target += losses.ssim(img_input, img_target).item()
@@ -171,6 +280,10 @@ if __name__ == "__main__":
                 plt.tight_layout()
 
                 plt.savefig([f'{name}/{name}_plot{args.batch_size*idx+j:06}.jpg' for j in range(frame.size(0))][0], bbox_inches='tight', pad_inches=0, dpi=500, pil_kwargs={'quality':95})
+                plt.close()
+                plt.cla()
+                plt.clf()
+            
             tq.update(args.batch_size)
         tq.close()
 
